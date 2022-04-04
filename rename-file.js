f2cf940344680067b45c10b81c595d492b05f43c
ChintzRuparel <chintz0952@gmail.com>
2022-04-04 14:03:13 +0530

    Rename File Added

---
+++ rename-file.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
+
+//Change File Name 
+//Change desired file name 
