0f70434466af58157a6fd2c08ae9faf8f68ea773
ChintzRuparel <chintz0952@gmail.com>
2022-04-09 15:43:48 +0530

    Trade Republic API added

---
+++ trade-republic.js
@@ -0,0 +1,11 @@
+const trapi = new TradeRepublicApi({});
+
+trapi.on('connected', async () => {
+  const data = await trapi.oneShot<Ticker>('ticker', {
+    id: 'US90364P1057.LSX',
+  });
+  console.log(JSON.stringify(data));
+  console.log(`Ask Price: ${data.payload.ask.price}`);
+});
+
+trapi.connect();
